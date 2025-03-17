06ee0930ba95551fa4b0e8a0d33466a10a5e853b
Quang Thang <thang@thangqt.com>
2025-03-17 17:36:47 +0700

    commit missing changes

--- sender_receiver/util.py
+++ sender_receiver/util.py
@@ -1,4 +1,5 @@
 import binascii
+import sys
 
 from scapy.all import Packet
 from scapy.all import IntField
@@ -6,7 +7,7 @@ from scapy.all import IntField
 class PacketHeader(Packet):
     name = "PacketHeader"
     fields_desc = [
-        IntField("type", 0),
+        IntField("type", 0), # 0: START; 1: END; 2: DATA; 3: ACK
         IntField("seq_num", 0),
         IntField("length", 0),
         IntField("checksum", 0),
@@ -14,3 +15,16 @@ class PacketHeader(Packet):
 
 def compute_checksum(pkt):
     return binascii.crc32(bytes(pkt)) & 0xffffffff
+
+def build_packet(header, data=b""):
+    header.checksum = 0
+    pkt_bytes = bytes(header) + data
+    header.checksum = compute_checksum(pkt_bytes)
+    return bytes(header) + data
+
+def create_ack(seq_num, ack_type=3):
+    ack_header = PacketHeader(type=ack_type, seq_num=seq_num, length=0, checksum=0)
+    return build_packet(ack_header)
+
+def print_debug(msg):
+    print(msg, file=sys.stderr)
